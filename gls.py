ba1356bd4bd35b34a3e407b23c85296900579b94
1Dambek1 <denolinevichd@inbox.ru>
2025-09-13 19:02:38 +0800

    code

--- gls.py
+++ gls.py
@@ -1,2 +1,17 @@
-impor hl
-print("hello world")
+import math
+
+a = int(input())
+b = int(input())
+
+print(math.e)
+def plus_e(a,b):
+ return a+b+math.e*2
+
+def mse_loss(a,b):
+ return  (a-b)**2
+
+def relu(a):
+ return 0 if a < 0 else a
+
+def sqr(a,b):
+ return a**2, b**2
